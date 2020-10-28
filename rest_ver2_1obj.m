ea9a12c6efbfb19b4e914840c94781d639f2cc6f
Cecília Eduarda Coelho <ceciliaeduarda@live.com.pt>
2020-10-28 17:45:54 +0000

    options tweaked so most algorithms can provide a solution

--- rest_ver2_1obj.m
+++ rest_ver2_1obj.m
@@ -10,7 +10,7 @@ Q_in = randi([100,1000],1,50); % inflow
 
 Q = pi * v(i) * x(1)^2; %turbine flow
 
-C = [ pi*x(1)^2*v(i)-400 , -(Vi-0.7*V_init) , -(pi*v(i)*x(1)^2-400) , Vi - 55e6];
+C = [ pi*x(1)^2*v(i)-400 , -(Vi-0.7*V_init) , -(pi*v(i)*x(1)^2-400) ];%, Vi - 55e6];
 
 Ceq = [];
 
