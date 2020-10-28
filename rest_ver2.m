9cdc180089674c88e6264d1d7d60e31c85f7ec3c
Cecília Eduarda Coelho <ceciliaeduarda@live.com.pt>
2020-10-28 11:32:54 +0000

    might be necessary to play around with the oprions of the algorithms

---
+++ rest_ver2.m
@@ -0,0 +1,21 @@
+function [C,Ceq] = rest_ver2(x,i,Vi)
+
+rng(1);
+%v = 6*ones(1,50); 
+v = randi([6,20],1,50);
+t = 3600; %delta t in seconds
+V_init = 50e6; %https://www.tandfonline.com/doi/pdf/10.1623/hysj.49.5.901.55139?needAccess=true
+Q_in = randi([100,1000],1,50); % inflow
+
+
+Q = pi * v(i) * x(1)^2; %turbine flow
+
+Q_spill = pi * v(i) * x(2)^2; %spill flow
+
+C = [ pi*x(1)^2*v(i)-400 , -(Vi-0.7*V_init) , -(pi*v(i)*x(1)^2+pi*v(i)*x(2)^2-400) , Vi - 55e6];
+
+Ceq = [];
+
+    
+end
+
