da6d3c7657a7d58695c698bf5768570b3ed16fa7
Cecília Eduarda Coelho <ceciliaeduarda@live.com.pt>
2020-11-16 18:41:14 +0000

    added 3rd variable to control the water speed when released from the storage

--- rest_ver2_1obj.m
+++ rest_ver2_1obj.m
@@ -1,16 +1,14 @@
-function [C,Ceq] = rest_ver2_1obj(x,i,Vi)
+function [C,Ceq] = rest_ver2_1obj(x,Vi)
 
 rng(1);
-%v = 6*ones(1,50); 
-v = randi([6,20],1,50);
 t = 3600; %delta t in seconds
 V_init = 50e6; %https://www.tandfonline.com/doi/pdf/10.1623/hysj.49.5.901.55139?needAccess=true
-Q_in = randi([100,1000],1,50); % inflow
+Q_in = randi([10,1000],1,50); % inflow
 
 
-Q = pi * v(i) * x(1)^2; %turbine flow
+Q = pi * x(2) * x(1)^2; %turbine flow
 
-C = [ pi*x(1)^2*v(i)-400 , -(Vi-0.7*V_init) , -(pi*v(i)*x(1)^2-400) ];%, Vi - 55e6];
+C = [ pi*x(1)^2*x(2)-400 , -(Vi-0.7*V_init) , -(pi*x(2)*x(1)^2-400) , Vi - 55e6];
 
 Ceq = [];
 
